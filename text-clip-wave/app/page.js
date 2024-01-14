93035785fa6aad8b81f623a00b62ce2ebc75207e
Abhinav Kumar <abhinavkumar1312004@gmail.com>
2024-01-14 08:56:52 +0530

    Added Text Clip Wave Effect

---
+++ text-clip-wave/app/page.js
@@ -0,0 +1,14 @@
+'use client'
+
+import { useEffect, useRef, useState } from 'react';
+import './style.css';
+export default function HomePage(){
+    return(<>
+        <div className="flex items-center justify-center self-center justify-self-center h-[100vh] w-[100vw]">
+            <div  className='p-0 m-0 flex items-center justify-center relative h-fit w-fit'>
+                <h1 className="p-0 m-0 text-[15em] font-bold text-transparent ">Wave</h1>
+                <h1 className="p-0 m-0 text-[15em] font-bold text-transparent ">Wave</h1>
+            </div>
+        </div>
+    </>)
+}
